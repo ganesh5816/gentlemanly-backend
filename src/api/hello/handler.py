6bee9a9bffc4490f70c444cea08a6e8f83e86120
Ganes769 <ganesh@skillrank.io>
2025-07-13 15:14:08 +0100

    init

---
+++ src/api/hello/handler.py
@@ -0,0 +1,8 @@
+import json
+def handler(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
